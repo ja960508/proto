d62dfc9b4814082078184e46296dc1d05ec04ada
jooam <ja960508@naver.com>
2021-01-26 11:24:45 +0900

    change list tag to table tag, photo layout

--- src/js/photo.js
+++ src/js/photo.js
@@ -1,8 +1,72 @@
 const targetUl = document.querySelector(".photo-layout");
 const enlargeBtn = document.querySelector(".enlargement-btn");
 const reductionBtn = document.querySelector(".reduction-btn");
+const targetTbody = document.querySelector("#js-tbody");
 
 let magnifyingGlassController = 0;
+
+const items = [
+  {
+    name: "202011241641-B711142-이종찬",
+  },
+  {
+    name: "202011241643-B711142-이종찬",
+  },
+  {
+    name: "202011251634-B411042-김영진",
+  },
+  {
+    name: "202011251634-B635453-한재현",
+  },
+  {
+    name: "202011251634-B911085-백지웅",
+  },
+  {
+    name: "202011251638-B711218-황선호",
+  },
+  {
+    name: "B311202-정찬용-202011261633",
+  },
+  {
+    name: "B390036-송주영-202011251639",
+  },
+  {
+    name: "B411119-안승태-202011241643",
+  },
+  {
+    name: "B411119-안승태-202011261634",
+  },
+  {
+    name: "B511042-김재환-202011261634",
+  },
+  {
+    name: "B611110-양민섭-202011241644",
+  },
+  {
+    name: "B611119-오준석-202011241643",
+  },
+  {
+    name: "B611156-이은-202011241645",
+  },
+  {
+    name: "B635453-한재현-202011251638",
+  },
+  {
+    name: "B711002-강소정-202011261634",
+  },
+  {
+    name: "B711012-길상현-202011261635",
+  },
+  {
+    name: "B711069-박선형-202011261637",
+  },
+  {
+    name: "B711107-양수민-202011261633",
+  },
+  {
+    name: "B811087-서예진-202011251636",
+  },
+];
 
 function enlargeImages() {
   const currentImages = document.querySelectorAll(".photo-items");
@@ -49,8 +113,7 @@ function reductionImages() {
 }
 
 function deleteList(event) {
-  const targetList = event.target.parentNode.parentNode.parentNode;
-  targetUl.removeChild(targetList);
+  targetTbody.removeChild(event.target.parentNode.parentNode.parentNode);
 }
 
 function currentEnlargement() {
@@ -65,37 +128,38 @@ function currentEnlargement() {
 
 function makeList() {
   const imageNumber =
-    targetUl.childElementCount / 10 >= 1
-      ? `${targetUl.childElementCount}`
-      : `0${targetUl.childElementCount}`;
-
-  const li = document.createElement("li");
+    targetTbody.childElementCount / 10 >= 1
+      ? `${targetTbody.childElementCount}`
+      : `0${targetTbody.childElementCount}`;
+  const tr = document.createElement("tr");
   currentEnlargement().map((item) => {
-    li.classList.add(item);
+    tr.classList.add(item);
   });
-  li.innerHTML = `
-    <div class="photo-num toslide-font-small-normal">${imageNumber}</div>
-    <div class="image-container">
-        <div class="image-info">
-            <div class="image-keyword toslide-font-small-medium">sample_keyword</div>
-            <div class="image-sender toslide-font-small-normal">B511153 Leejooam</div>
-        </div>
+  tr.innerHTML = `
+    <td class="photo-num toslide-font-small-normal">${imageNumber}</td>
+    <td class="image-container">
         <div class="image-section">
-            <img src="./src/images/sample${
-              targetUl.childElementCount % 6
+            <img src="./src/images/sample_photos/${
+              items[targetTbody.childElementCount % 20].name
             }.jpg" class="image-section-image">
-            <div class="image-title toslide-font-small-lineheight20-normal">sample_image${imageNumber}.jpg</div>
+            <div class="image-title toslide-font-small-lineheight20-normal">${
+              items[targetTbody.childElementCount % 20].name
+            }.jpg</div>
         </div>
-    </div>
-    <div class="btn-container">
+    </td>
+    <td class="btn-container">
         <button class="photo-delete-btn" onclick="deleteList(event)">
             <img src="./src/images/photo_section/PNG_cancle_gray.png" class="delete-btn-image">
         </button>
-    </div>
+    </td>
     `;
-
-  targetUl.appendChild(li);
+  targetTbody.appendChild(tr);
 }
+
+//     <div class="image-info">
+//     <div class="image-keyword toslide-font-small-medium">sample_keyword</div>
+//     <div class="image-sender toslide-font-small-normal">B511153 Leejooam</div>
+//     </div>
 
 const sampleBtn = document.querySelector(".make-sample");
 sampleBtn.addEventListener("click", makeList);
