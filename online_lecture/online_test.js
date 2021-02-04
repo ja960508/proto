4396998870815eb2b4e0209ea2c14ba365be55e8
jooam <ja960508@naver.com>
2021-02-05 01:33:04 +0900

    chore: make new directory for online_lecture - css, images, js files... moved into new folder

--- online_lecture/online_test.js
+++ online_lecture/online_test.js
@@ -32,7 +32,8 @@ function openTestSection(input) {
 
   hideSection.classList.add("d-none");
   testSectionWrap.classList.remove("d-none");
-  testFileIframe.src = input.value;
+  testFileIframe.src =
+    "https://docs.google.com/document/d/1E_0U7w2f_kJO0SCSPeccJdNBc9wpZWeCowgOmWP_Amk/edit";
 }
 
 testStartBtn.addEventListener("click", () => {
