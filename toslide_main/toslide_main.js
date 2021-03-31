9aa2ae4fb97a859224b08dcc375cd4e61116b80f
jooam <ja960508@naver.com>
2021-03-31 17:01:25 +0900

    make translate func

--- toslide_main/toslide_main.js
+++ toslide_main/toslide_main.js
@@ -5,47 +5,30 @@ const selectLecturer = document.querySelector("#select_lecturer");
 const selectStudent = document.querySelector("#select_student");
 /* 동영상 링크 정보 관리 */
 const QA_LECTURER = [
-  ['질의응답모드 둘러보기', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['메시지', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['사진', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['투표', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["질의응답모드 둘러보기", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["메시지", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["사진", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["투표", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
 const QA_STUDENT = [
-  ['메시지 보내기', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['사진 보내기', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['투표하기', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["메시지 보내기", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["사진 보내기", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["투표하기", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
 const LEC_LECTURER = [
-  ['강의모드 둘러보기', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['강의모드 활용하기', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["강의모드 둘러보기", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["강의모드 활용하기", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
 const LEC_STUDENT = [
-  ['강의모드 활용하기', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["강의모드 활용하기", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
 const TEST_LECTURER = [
-  ['시험모드 둘러보기', "https://www.youtube.com/embed/ruIciRNDrDA"],
-  ['시험모드 활용하기', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["시험모드 둘러보기", "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["시험모드 활용하기", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
 const TEST_STUDENT = [
-  ['시험모드 활용하기', "https://www.youtube.com/embed/ruIciRNDrDA"],
+  ["시험모드 활용하기", "https://www.youtube.com/embed/ruIciRNDrDA"],
 ];
-
-function textTranslate(target) {
-  const korean = document.querySelector(".translate__korean");
-  const english = document.querySelector(".translate__english");
-  switch (target.textContent) {
-    case "한국어":
-      !korean.classList.contains("active") &&
-        english.classList.remove("active");
-      !korean.classList.contains("active") && korean.classList.add("active");
-      break;
-    case "English":
-      !english.classList.contains("active") &&
-        korean.classList.remove("active");
-      !english.classList.contains("active") && english.classList.add("active");
-      break;
-  }
-}
 
 function selectTutorialContent(target) {
   let selectedContent;
@@ -68,23 +51,27 @@ function selectTutorialContent(target) {
   getTutorialImageContent(selectedContent, target);
 }
 /* Add for videos */
-function addVideoBox( videoList ) {
-  for( videoInfo of videoList ) {
-    if (videoInfo === undefined)
-      continue;
-    const div = document.createElement('div');
-    div.className = 'video-box';
-    div.innerHTML = `
-      <figcaption>`+videoInfo[0]+`</figcaption>
-      <iframe src="`+videoInfo[1]+`"frameborder="0" allowfullscreen class="video">
+function addVideoBox(videoList) {
+  for (videoInfo of videoList) {
+    if (videoInfo === undefined) continue;
+    const div = document.createElement("div");
+    div.className = "video-box";
+    div.innerHTML =
+      `
+      <figcaption>` +
+      videoInfo[0] +
+      `</figcaption>
+      <iframe src="` +
+      videoInfo[1] +
+      `"frameborder="0" allowfullscreen class="video">
       </iframe>
     `;
-    document.getElementById('tutorial-videos').appendChild(div);
+    document.getElementById("tutorial-videos").appendChild(div);
   }
 }
 function removeRow() {
-  const videos = document.getElementById('tutorial-videos');
-  videos.innerHTML = '';
+  const videos = document.getElementById("tutorial-videos");
+  videos.innerHTML = "";
 }
 function getTutorialImageContent(
   target,
@@ -102,19 +89,19 @@ function getTutorialImageContent(
         joinerMode.classList.add("d-none");
       break;
     case tutorialSectionModeItems[1].innerText:
-      // 탭 공통 영역 
+      // 탭 공통 영역
       imgContent.src = "./images/qa_benefit.jpg";
       // 탭 개별 영역
       if (selectedJoiner === selectLecturer) {
         addVideoBox(QA_LECTURER);
         // Additional image
-        const div = document.createElement('div');
-        div.className = 'video-box';
+        const div = document.createElement("div");
+        div.className = "video-box";
         div.innerHTML = `
         <figcaption>질의응답 모바일 컨트롤</figcaption>
         <img src="./images/qa_control.jpg" class="video" height="100%" />
         `;
-        document.getElementById('tutorial-videos').appendChild(div);
+        document.getElementById("tutorial-videos").appendChild(div);
       } else {
         addVideoBox(QA_STUDENT);
       }
@@ -123,7 +110,7 @@ function getTutorialImageContent(
         joinerMode.classList.remove("d-none");
       break;
     case tutorialSectionModeItems[2].innerText:
-      // 탭 공통 영역 
+      // 탭 공통 영역
       imgContent.src = "./images/lecture_benefit.jpg";
       // 탭 개별 영역
       if (selectedJoiner === selectLecturer) {
@@ -136,7 +123,7 @@ function getTutorialImageContent(
         joinerMode.classList.remove("d-none");
       break;
     case tutorialSectionModeItems[3].innerText:
-      // 탭 공통 영역 
+      // 탭 공통 영역
       imgContent.src = "./images/test_benefit.jpg";
       // 탭 개별 영역
       if (selectedJoiner === selectLecturer) {
---
+++ toslide_main/toslide_main_translate.js
@@ -0,0 +1,47 @@
+function textTranslate(target) {
+  const korean = document.querySelector(".translate__korean");
+  const english = document.querySelector(".translate__english");
+  const domArray = [
+    document.querySelector("#lbutton"),
+    document.querySelector("#sbutton"),
+    document.querySelector("#toslide_intro"),
+    document.querySelector("#toslide_qna"),
+    document.querySelector("#toslide_online_lecture"),
+    document.querySelector("#toslide_online_test"),
+    document.querySelector("#select_lecturer"),
+    document.querySelector("#select_student"),
+  ];
+
+  const koreanText = [
+    "강사로 참여하기",
+    "학생으로 참여하기",
+    "투슬라이드 소개",
+    "메시지 및 사진으로 질의응답",
+    "파일을 공유하는 온라인 강의",
+    "부정행위 방지하는 온라인 시험",
+    "강사입장",
+    "학생입장",
+  ];
+  const engText = ["a", "a", "a", "a", "a", "a", "a", "a"];
+
+  switch (target.textContent) {
+    case "한국어":
+      !korean.classList.contains("active") && translate(domArray, koreanText);
+      !korean.classList.contains("active") &&
+        english.classList.remove("active");
+      !korean.classList.contains("active") && korean.classList.add("active");
+      break;
+    case "English":
+      !english.classList.contains("active") && translate(domArray, engText);
+      !english.classList.contains("active") &&
+        korean.classList.remove("active");
+      !english.classList.contains("active") && english.classList.add("active");
+      break;
+  }
+}
+
+function translate(domArray, textContent) {
+  for (let i = 0; i < domArray.length; i++) {
+    domArray[i].innerText = textContent[i];
+  }
+}
